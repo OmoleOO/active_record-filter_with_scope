13a135898ab042ea1c52a9c9fd8998c5249367c5
OmoleOO <olumideomole@gmail.com>
2023-09-23 22:33:51 +0200

    Rewrite unclear example

--- test/active_record/filter_with_scope/config_test.rb
+++ test/active_record/filter_with_scope/config_test.rb
@@ -16,12 +16,12 @@ module ActiveRecord
 
       def test_should_map_filter_keys_to_model_scope
         Job.filter_with filter_keys: @filter_keys, scope_prefix: "with_" do |scope_for|
-          scope_for.title = :with_similar_title
+          scope_for.title = :similar_title
         end
 
         mapping = filterable_model_scope_mapping
 
-        assert_includes mapping.values, :with_similar_title
+        assert_includes mapping.values, :similar_title
         assert(mapping.except(:title).values.all? { |scope| scope.to_s.start_with?("with_") })
       end
 
